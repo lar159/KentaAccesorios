a51d44971b0b005c885bb3d1a6fea7e38ca16ea6
lar159 <luu.rivadeneira@gmail.com>
2023-10-21 20:29:27 -0300

    se realizo el scaffolding y se instalaron las dependencias

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { Routes, BrownserRouter, Route} from 'react-router-dom'
+import Home from "./components/pages/Home/Home"
+import Login from "./components/pages/Login/Login"
+import Register from "./components/pages/Register/Register"
+import Error from "./components/pages/Error/Error"
+import AboutUs from "./components/pages/AboutsUs/AboutUs"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <BrownserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/aboutUs" element={<AboutUs />} />
+          <Route exact path="/*" element={<Error />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </BrownserRouter>
+    </>
+  )
+}
+
+export default App
